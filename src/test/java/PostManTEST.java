82f5452f8f0d82459e0dcb86afddba8008effa27
Nikita Chalyi <nikita4556@gmail.com>
2024-05-11 20:12:34 +0300

    reformate

--- src/test/java/PostManTEST.java
+++ src/test/java/PostManTEST.java
@@ -1,25 +1,26 @@
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PostManTEST {
     @Test
-
-     void shouldPostDemoAccounts(){
+    void shouldPostDemoAccounts() {
 
 
         // Given - When - Then
 // Предусловия
-    given()
-  .baseUri("https://postman-echo.com")
-  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
-.when()
-  .post("/post")
+                .when()
+                .post("/post")
 // Проверки
-.then()
-  .statusCode(200)
-  .body("data",equalTo("some data"))
-            ;
-}
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
 }
 
